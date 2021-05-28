90ff00b65ba9443ecad0ed79d9a395f13190a5d6
shubhambeast <43602057+shubhamuwu@users.noreply.github.com>
2021-05-28 10:50:53 +0530

    Create logistic regression.py

---
+++ logistic regression.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+ds=dataset_csv("Social_Network_Ads.csv")
+
+
+from sklearn.model_selection import test_train_split
+x_train,x_test,y_train,y_test
